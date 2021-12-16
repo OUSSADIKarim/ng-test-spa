f19a4cfc4232a3fbae62b7aa6192cedcff63156e
OUSSADI Karim <oussadikarim00@gmail.com>
2021-12-16 04:50:59 +0100

    eightest commit

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@
 (function () {
     function init() {
         var router = new Router([
-            new Route('home', 'home.html', true),            
+            new Route('home', 'home.html', true),    
+            new Route('services', 'services.html'),        
+            new Route('gallery', 'gallery.html'),        
             new Route('contact', 'contact.html')
         ]);
     }
